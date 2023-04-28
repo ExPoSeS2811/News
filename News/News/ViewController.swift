583b15a6bb2de8542975bbda267d2d5c87e57d49
Gleb Rasskazov <glebunium146@gmail.com>
2023-04-28 23:08:40 +0300

    Initial commit

---
+++ News/News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by MacBook Pro 13 2019 on 4/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
